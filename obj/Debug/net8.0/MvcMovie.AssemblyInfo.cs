646e6a726dd057448aacde22db65c5c0c1342ab2
Ayman Qaisari <qaisariayman@gmail.com>
2025-10-04 19:54:44 +0200

    Second commit(GFQS7d): Add a Controller

--- obj/Debug/net8.0/MvcMovie.AssemblyInfo.cs
+++ obj/Debug/net8.0/MvcMovie.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8a2665911005a8af2db48fe8559e54f3b21de593")]
 [assembly: System.Reflection.AssemblyProductAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
